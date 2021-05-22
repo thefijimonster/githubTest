e2b1960ea91fb70afab998da5b9b8e376536b921
thefijimonster <david.chen02@icloud.com>
2021-05-22 18:04:13 +0100

    Initial Commit

---
+++ githubTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  githubTest
+//
+//  Created by David Chen on 22/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ githubTestTests/githubTestTests.swift
@@ -0,0 +1,33 @@
+//
+//  githubTestTests.swift
+//  githubTestTests
+//
+//  Created by David Chen on 22/05/2021.
+//
+
+import XCTest
+@testable import githubTest
+
+class githubTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
